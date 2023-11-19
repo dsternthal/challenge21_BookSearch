ab2ef61c3ffb7b524412fec5c5a85d5b27736c11
Dani Sternthal <dstrnthl97@gmail.com>
2023-11-19 16:51:59 -0500

    save

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,10 +37,10 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
